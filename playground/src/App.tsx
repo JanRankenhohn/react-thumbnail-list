285bfdefafce1f19447033be65fec2e74a92af46
janrankenhohn <j.rankenhohn@gmail.com>
2024-02-29 16:42:28 +0100

    switched playground to vite

---
+++ playground/src/App.tsx
@@ -0,0 +1,23 @@
+import {HelloWorld} from 'react-thumbnail-list';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <HelloWorld />
+    </>
+  )
+}
+
+export default App
+
+// import './App.css';
+// import {HelloWorld} from 'react-thumbnail-list';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <HelloWorld />
+//     </div>
+//   );
+// }
